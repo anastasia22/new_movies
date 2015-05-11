b89def3c6c9d728929cace48a3588d17312522ce
anastasia22 <abuniak22@gmail.com>
2015-05-11 23:50:10 +0300

    started grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,25 @@
 var grunt = require('grunt');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-livereload');
+
+var path = {};
+path.scripts = 'app/scripts/**';
+path.sass = 'app/styles/sass/**';
+path.views = 'app/views/*';
+
+grunt.initConfig({
+  watch: {
+	  scripts: 
+	  {
+	    files: [path.scripts, path.views],
+	    tasks: ['world']
+	  },
+	  css: {
+  		files: [path.sass],
+	    tasks: ['world']
+	  }
+
+});
 
 grunt.registerTask('world', 'world task description', function(){
   console.log('hello world');
@@ -11,4 +32,5 @@ grunt.registerTask('hello', 'say hello', function(name){
   console.log('hello ' + name);
 });
 
-grunt.registerTask('default', ['world', 'hello:adrian']);
+
+grunt.registerTask('default', ['world', 'hello:adrian', 'watch']);
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,23 @@
-'use strict'
+'use strict';
 
 angular.module('springMovies', [
 	'ngRoute'
 ])
 
 
-// .config(function($routeProvider, $locationProvider) {
-//   $routeProvider
-//    .when('/', {
-//     templateUrl: 'views/ .html',
-//     controller: 'Controller'
-//   })
-//   .when('/ /:id', {
-//     templateUrl: 'views/ .html',
-//     controller: 'Controller'
-//   });
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+   .when('/', {
+    templateUrl: '../views/main_page.html'/*,*/
+    //controller: 'Controller'
+  })
+   .when('/movies', {
+    templateUrl: '../views/movies.html'/*,*/
+    //controller: 'Controller'
+  })
+  .when('/movie/:id', {
+    templateUrl: '../views/movie.html'/*,*/
+    //controller: 'Controller'
+  })
+    
+ }); 
